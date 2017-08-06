d66a6b03dc3cf1859ae7eab7489e575c10b27bf3
Julio Alorro <alorro3@gmail.com>
2017-08-06 23:23:40 +0800

    Added TableViewCellDSL

---
+++ Sources/Extensions/UITableViewCell+Alacrity.swift
@@ -0,0 +1,16 @@
+//
+//  Alacrity
+//  Copyright (c) 2017 Julio Miguel Alorro
+//  Licensed under the MIT license. See LICENSE file
+//
+
+import UIKit
+
+public extension UITableViewCell {
+    /**
+     AlacrityTableViewCellDSL instance to access the Alacrity fluent interface
+     */
+    var acy: AlacrityTableViewCellDSL {
+        return AlacrityTableViewCellDSL(view: self)
+    }
+}
